5b73675003409d0ca7285fcea57a346f4dc9366b
izacarmo93@gmail.com <izacarmo93@gmail.com>
2025-11-06 19:03:50 -0300

    remove package-lock

--- app.js
+++ app.js
@@ -1,13 +1,12 @@
 const express = require ("express");
 const app = express();
-const {} = require("./src/routes/produtoRoutes");
-const produtoRoutes = require("./src/routes/produtoRoutes");
+const {produtoRoutes} = require("./src/routes/produtoRoutes");
 const PORT = 8081;
 
 app.use(express.json());
 
-app.use('/', produtoRoutes)
+app.use('/', produtoRoutes);
 
 app.listen(PORT, ()=>{
-    console.log('Servidor rodando em http://localhost:$(PORT)');
-})
+    console.log(`Servidor rodando em http://localhost:${PORT}`);
+});
